3e44ddbdcbd9005d4f489da896f6947d6203fbdb
ROCIO <ROCIO@HERNANDEZ>
2021-05-14 02:07:16 -0500

    Tarea 3 Ejercicio 2 :)

---
+++ src/tarea3ejercicio2/Tarea3Ejercicio2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea3ejercicio2;
+
+/**
+ *
+ * @author ROCIO
+ */
+public class Tarea3Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float n = 1;
+        float numero;
+        for(float i = 103;i>0;i--){
+            n=n*i;
+        }System.out.println("El factorial de 103 es " + n);
+    }
+    
+}
